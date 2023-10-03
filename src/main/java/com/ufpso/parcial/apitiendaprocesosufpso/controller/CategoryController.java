30e88189205ab07e5078b190a9dd5cc26caf430f
Jhoiner <jjmartinezs@ufpso.edu.co>
2023-10-02 21:08:38 -0500

    IMPLEMENTACION - End Points categorias
    - Se agregaron los end points para las categorias

--- src/main/java/com/ufpso/parcial/apitiendaprocesosufpso/controller/CategoryController.java
+++ src/main/java/com/ufpso/parcial/apitiendaprocesosufpso/controller/CategoryController.java
@@ -1,7 +1,41 @@
 package com.ufpso.parcial.apitiendaprocesosufpso.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ufpso.parcial.apitiendaprocesosufpso.model.Category;
+import com.ufpso.parcial.apitiendaprocesosufpso.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("category/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(id));
+    }
+
+    @GetMapping("category/list")
+    public ResponseEntity<List<Category>> getAllCategory() {
+        return ResponseEntity.ok(categoryService.getAllCategory());
+    }
+
+    @PostMapping("category")
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.createCategory(category), HttpStatus.CREATED);
+    }
+
+    @PutMapping("category/update/{id}")
+    public ResponseEntity<Category> update(@RequestBody Category category, @PathVariable Long id ) {
+        return new ResponseEntity<>(categoryService.updateCategory(category,id),HttpStatus.OK);
+    }
+
+    @DeleteMapping("category/{id}")
+    public ResponseEntity<List<Category>> delete(@PathVariable Long id ) {
+        return new ResponseEntity(categoryService.deleteCategory(id),HttpStatus.NO_CONTENT);
+    }
 }
